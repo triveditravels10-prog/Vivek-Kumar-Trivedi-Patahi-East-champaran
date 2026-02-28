699d48f01153c9814160f27608e4766cca124815
triveditravels10-prog <triveditravels10@gmail.com>
2026-02-28 17:04:35 +0530

    Create hospital.cpp

---
+++ hospital.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string patient;
+    cout << "Enter patient name: ";
+    cin >> patient;
+    cout << "Patient Registered: " << patient << endl;
+    return 0;
+}
