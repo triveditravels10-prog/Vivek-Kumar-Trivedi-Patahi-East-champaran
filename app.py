36283f6bfca9c3b2f04a5adac6223e45fd9f9728
triveditravels10-prog <triveditravels10@gmail.com>
2026-03-03 22:24:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Trivedi Hospital Backend Running"
+    return "Website Running Successfully!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
