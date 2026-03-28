d8ae1a016680954706fa75b15482cfe9878cbe4a
triveditravels10-prog <triveditravels10@gmail.com>
2026-03-28 17:27:35 +0530

    Remove visitor counter from script.js
    Removed visitor counter functionality from script.js.

--- script.js
+++ script.js
@@ -5,22 +5,6 @@
 
 window.addEventListener("load", function(){
 
-  // Visitor Counter
-  let count = localStorage.getItem("visits");
-
-  if(count === null){
-    count = 1;
-  } else {
-    count = parseInt(count) + 1;
-  }
-
-  localStorage.setItem("visits", count);
-
-  let countBox = document.getElementById("count");
-  if(countBox){
-    countBox.innerText = "Total Visits: " + count;
-  }
-
   // Clock
   function updateClock(){
     let clock = document.getElementById("clock");
