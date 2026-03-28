804fbd9453bcd01471d89173e652e720938491da
triveditravels10-prog <triveditravels10@gmail.com>
2026-03-28 17:16:10 +0530

    Remove scroll event listener for reveals
    Removed scroll event listener for reveal elements.

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-window.addEventListener("scroll", function() {
-    let reveals = document.querySelectorAll(".reveal");
 
-    for (let i = 0; i < reveals.length; i++) {
-        let windowHeight = window.innerHeight;
-        let elementTop = reveals[i].getBoundingClientRect().top;
-        let elementVisible = 150;
-
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        }
-    }
-});
 
 
 
